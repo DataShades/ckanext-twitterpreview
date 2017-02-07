ab702ed5c63c550dcd06c72482adf0c668eaf2cc
Yan <yan.rudneko@linkdigital.com.au>
2017-02-07 11:50:04 +0200

    Use Embedded Timelines for twitter

--- ckanext/twitter_feeds/plugin.py
+++ ckanext/twitter_feeds/plugin.py
@@ -1,35 +1,12 @@
-# CKAN modules
-
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan import plugins as p
 import ckan.logic as logic
 
-# Twitter modules
-from ttp import ttp
-import twitter
-
-# Other modules
 from pylons import config
-from datetime import datetime
-
-p2 = ttp.Parser()
 
 ignore_empty = p.toolkit.get_validator('ignore_empty')
 DEFAULT_TWIITER_FORMATS = ['TWITTER FEED']
-
-twitter_api = twitter.api.Api(
-    consumer_key=config.get('ckan.twitter.consumer_key'),
-    consumer_secret=config.get('ckan.twitter.consumer_secret'),
-    access_token_key=config.get('ckan.twitter.access_token_key'),
-    access_token_secret=config.get('ckan.twitter.access_token_secret')
-)
-
-
-def twitter_feed_date(feed_date):
-    date = datetime.strptime(
-        feed_date, "%a %b %d %H:%M:%S +0000 %Y").strftime('%b %d')
-    return date
 
 
 def twitter_feed_validation(resource):
@@ -41,14 +18,6 @@ def twitter_feed_validation(resource):
             else:
                 message = [
                     'This Url is wrong for this resource format. The Url should match the example: "https://twitter.com/USERNAME"']
-                raise logic.ValidationError({"Twitter": message})
-        try:
-            test = twitter_api.GetUserTimeline(
-                screen_name=resource['url'].rsplit('twitter.com/', 1)[1],
-                count=1)
-        except twitter.TwitterError, e:
-            for m in e.message:
-                message = [m['message']]
                 raise logic.ValidationError({"Twitter": message})
 
 
@@ -70,7 +39,6 @@ class Twitter_FeedsPlugin(plugins.SingletonPlugin):
 
     def get_helpers(self):
         return {
-            'twitter_feed_date': twitter_feed_date,
             'twitter_feed_validation': twitter_feed_validation
         }
 
@@ -99,84 +67,16 @@ class Twitter_FeedsPlugin(plugins.SingletonPlugin):
 
     def view_template(self, context, data_dict):
         if data_dict['resource']['format'].upper() in DEFAULT_TWIITER_FORMATS:
-            feeds = []
-            exclude_replies = False
-            exclude_retweet = False
-            if config.get('ckan.twitter.include_retweets') == 'True':
-                exclude_retweet = True
+            replies = False
             if config.get('ckan.twitter.exclude_replies') == 'True':
-                exclude_replies = True
+                replies = True
 
-            twitter_info = twitter_api.GetUserTimeline(
-                screen_name=data_dict['resource'][
-                    'url'].rsplit('twitter.com/', 1)[1],
-                count=config.get('ckan.twitter.max_feeds_count'),
-                include_rts=exclude_retweet,
-                exclude_replies=exclude_replies,
-            )
-
-            for feed in twitter_info:
-                retweeted = False
-                reply = False
-                if feed.in_reply_to_screen_name:
-                    reply = True
-                if feed.retweeted_status:
-                    retweeted = True
-                    text = feed.retweeted_status.text
-                else:
-                    text = feed.text
-
-                if 'https://t.co/' in text:
-                    last_url = text.rsplit('https://t.co/', 1)[1].split()
-                    last_url = last_url[0]
-
-                    if text.endswith(last_url):
-                        last_url = 'https://t.co/' + last_url
-                        text = text.replace(last_url, '')
-
-                complete_text = p2.parse(text)
-                feed = {
-                    'id': (
-                        feed.retweeted_status.id if
-                        feed.retweeted_status else feed.id),
-                    'created_at': feed.created_at,
-                    'retweet_count': feed.retweet_count,
-                    'text': complete_text.html,
-                    'user': {
-                        'name': feed.user.name,
-                        'id': feed.user.id,
-                        'screen_name': feed.user.screen_name
-                    },
-                    'twitt_info': {
-                        'id': (
-                            feed.retweeted_status.user.id if
-                            feed.retweeted_status else feed.user.id),
-                        'created_at': (
-                            feed.retweeted_status.user.created_at if
-                            feed.retweeted_status else feed.user.created_at),
-                        'description': (
-                            feed.retweeted_status.user.description if
-                            feed.retweeted_status else feed.user.description),
-                        'name': (
-                            feed.retweeted_status.user.name if
-                            feed.retweeted_status else feed.user.name),
-                        'profile_image_url': (
-                            feed.retweeted_status.user.profile_image_url if
-                            feed.retweeted_status else feed.user.profile_image_url),
-                        'screen_name': (
-                            feed.retweeted_status.user.screen_name if
-                            feed.retweeted_status else feed.user.screen_name),
-                        'statuses_count': (
-                            feed.retweeted_status.user.statuses_count if
-                            feed.retweeted_status else feed.user.statuses_count)
-                    },
-                    'retweeted': retweeted,
-                    'reply': reply
-                }
-                feeds.append(feed)
-
-            data_dict['resource']['twitt_feeds_count'] = len(twitter_info)
-            data_dict['resource']['twitt_feeds'] = feeds
+            data_dict['resource']['count_tweets'] = config.get(
+                'ckan.twitter.max_feeds_count')
+            user_screen_name = data_dict['resource'][
+                'url'].rsplit('twitter.com/', 1)[1]
+            data_dict['resource']['user_screen_name'] = user_screen_name
+            data_dict['resource']['tweet_replies'] = replies
         return 'twitter_feed_view.html'
 
     def form_template(self, context, data_dict):
